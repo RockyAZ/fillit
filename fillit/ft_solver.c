467bc7bb265e46ebc91b732abf2206409d47450b
Anton ZAPOROZHCHENKO <azaporoz@e2r12p14.unit.ua>
2018-04-23 19:45:26 +0300

    work with str 10

--- fillit/ft_solver.c
+++ fillit/ft_solver.c
@@ -34,9 +34,10 @@ void	ft_printmap(t_res *map, t_pos *pos)
 			lst_pos++;
 		}
 		j = 0;
+		i = 0;
 		pos = pos->next;
 	}
-	printf("map :\n%s\n", map->map);
+	printf("map :\n********************\n%s\n****************\n", map->map);
 }
 
 void	ft_mover(t_pos *cp, int bul)
@@ -76,7 +77,7 @@ int		is_map(t_pos *cp, int size)
 }
 
 int		ft_rec_solv(t_pos *beg, t_pos *pos, int size, int to)
-{
+{//write(1, "1\n", 2);
 	int i;
 
 	if (pos == NULL)
@@ -115,8 +116,6 @@ void	ft_solver(t_pos *cp)
 	int bul;
 
 t_pos *cpp = cp;
-
-
 	map = NULL;
 	size = 2;
 	map_c = 6;
@@ -132,45 +131,42 @@ t_pos *cpp = cp;
 		cpp = cpp->next;
 	}
 cpp = cp;
-int i;
+/*------------------------------------------------*/
 	while (cpp != NULL)
 	{
 		printf("START:\n%s\n", cpp->content);
 		ft_rewrite(cpp, cpp);
 		printf("MOVER:\n%s\n", cpp->content);
 		printf("----------------------------\n");
-
 		cpp = cpp->next;
 	}
-
+/*------------------------------------------------*/
 
 	while (bul == 0)
-	{
-		map = ft_create_map(&map, map_c, size);
+	{//write(1, "AA\n", 3);
+//		map = ft_create_map(&map, map_c, size);
 		bul = ft_rec_solv(cp, cp, size, 1);
-		map_c += ad;
-		ad += 2;
-		size++;
+		if (!bul)
+		{
+			map = ft_create_map(&map, map_c, size);
+			map_c += ad;
+			ad += 2;
+			size++;
+		}
 	}
-//	ft_printmap(map, cp);
+		printf("MAAAAAAP :\n%s\n", map->map);
+
+	ft_printmap(map, cp);
 
 
-
-
-
-
-
-
-
-
-
-	i = 0;
+/*----------------------------------------------*/
+	int i = 0;
 	cpp = cp;
 	while (cpp != NULL)
 	{
 		ft_rewrite(cpp, cpp);
 		printf("END:\n%s\n", cpp->content);
-/*		while (i < 4)
+		while (i < 4)
 		{
 			ft_putstr("CPP_x- ");
 			ft_putnbr(cpp->x[i]);
@@ -180,7 +176,7 @@ int i;
 			write(1, "\n\n", 2);
 			i++;
 		}
-*/		printf("----------------------------\n");
+		printf("----------------------------\n");
 		i = 0;
 		cpp = cpp->next;
 	}
